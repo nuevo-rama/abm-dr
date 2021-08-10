c0f2bc204e8f541352ca84bc67bac64d320fb84b
nuevo-rama <ramiromelero@gmail.com>
2021-08-10 17:54:57 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # abm-dr
 ABM Node MySQL
+
+----
+
+# Guide
+
+1. npm init -y
+2. npm install express
+3. npm install mysql
+
---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express'),
+      path = require('path'),
+      mysql = require('mysql');
+
+const app = express();
+
+// importing routes
+const userRoutes = require('./routes/user');
+
+// settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+//app.set('view engine', 'ejs');
+
+// middlewares
+app.use(express.urlencoded({extended: false}));
+
+// routes
+app.use('/', userRoutes);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// starting the server
+app.listen(app.get('port'), () => {
+  console.log(`server on port ${app.get('port')}`);
+});
